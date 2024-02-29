66b9d83d642c815d78a2a1ec501098196475051a
ShimFFF <shimseawon1510@gmail.com>
2024-02-29 14:34:30 +0900

    :sparkles: Feat: add exception

---
+++ src/main/java/com/practice/spring/global/common/exception/ErrorCode.java
@@ -0,0 +1,13 @@
+package com.practice.spring.global.common.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Builder
+public class ErrorCode {
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
---
+++ src/main/java/com/practice/spring/global/common/exception/ErrorCodeInterface.java
@@ -0,0 +1,5 @@
+package com.practice.spring.global.common.exception;
+
+public interface ErrorCodeInterface {
+    ErrorCode getErrorCode();
+}
